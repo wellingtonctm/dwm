31493216cf1587526dfb266e03ae8045c06a9813
wellingtonctm <w.luiscm@gmail.com>
2025-05-14 16:05:38 -0300

    add gap configuration options and keybindings for managing gaps

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 5;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 5;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -90,6 +95,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period,                focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,                 tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,                tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_z,                     incrgaps,       {.i = +3 } },
+	{ MODKEY,                       XK_x,                     incrgaps,       {.i = -3 } },
+	{ MODKEY,                       XK_a,                     togglegaps,     {0} },
+	{ MODKEY|ShiftMask,             XK_a,                     defaultgaps,    {0} },
 	{ MODKEY,                       XK_y,                     spawn,          SHCMD("~/suckless/custom/plugins/dmenu/yt-cli.sh") },
 	{ MODKEY|ShiftMask,             XK_b,                     spawn,          SHCMD("~/suckless/custom/plugins/dmenu/bluetooth.sh") },
 	{ 0,                            XF86XK_AudioRaiseVolume,  spawn,          SHCMD("volume-plugin -i 5") },

99bc8e86730735215ce6952487ac8c3d39703fab
wellingtonctm <w.luiscm@gmail.com>
2025-07-10 14:09:10 -0300

    ajuste o tamanho da fonte para 11 no Noto Sans CJK KR

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {
 	"JetBrainsMono Nerd Font:size=11:antialias=true:autohint=true",
-	"Noto Sans CJK KR:size=10:antialias=true:autohint=true"
+	"Noto Sans CJK KR:size=11:antialias=true:autohint=true"
 };
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#000000";

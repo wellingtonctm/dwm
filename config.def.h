26c515dd0d57fa7c2c19b744420efeb5e131d82b
wellingtonctm <w.luiscm@gmail.com>
2025-07-10 13:56:04 -0300

    ajuste os comandos de brilho para aumentar e diminuir em 5% e melhore a configuração da fonte

--- config.def.h
+++ config.def.h
@@ -10,8 +10,10 @@ static const unsigned int gappov    = 5;       /* vert outer gap between windows
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=11";
+static const char *fonts[]          = {
+	"JetBrainsMono Nerd Font:size=11:antialias=true:autohint=true",
+	"Noto Sans CJK KR:size=10:antialias=true:autohint=true"
+};
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#000000";
 static const char col_gray3[]       = "#bbbbbb";
@@ -79,7 +81,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[] = { "st", NULL };
 static const char *browsercmd[] = { "brave-browser-stable", NULL };
 static const char *volumecentercmd[] = { "pavucontrol-qt", NULL };
@@ -136,8 +138,8 @@ static const Key keys[] = {
 	{ 0,                            XF86XK_AudioLowerVolume,  spawn,             SHCMD("volume-plugin -d 5") },
 	{ 0,                            XF86XK_AudioMute,         spawn,             SHCMD("volume-plugin -t") },
 	{ 0,                            XF86XK_AudioMicMute,      spawn,             SHCMD("volume-plugin -m") },
-	{ 0,                            XF86XK_MonBrightnessUp,   spawn,             SHCMD("brightnessctl set +1%") },
-	{ 0,                            XF86XK_MonBrightnessDown, spawn,             SHCMD("brightnessctl set 1%-") },
+	{ 0,                            XF86XK_MonBrightnessUp,   spawn,             SHCMD("brightnessctl set +5%") },
+	{ 0,                            XF86XK_MonBrightnessDown, spawn,             SHCMD("brightnessctl set 5%-") },
 	{ 0,                            XF86XK_AudioPlay,         spawn,             SHCMD("yt-cli --toggle") },
 	{ 0,                            XF86XK_AudioPause,        spawn,             SHCMD("yt-cli --toggle") },
 	{ 0,                            XF86XK_AudioNext,         spawn,             SHCMD("yt-cli --next") },
